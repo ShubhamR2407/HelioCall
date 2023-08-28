266a4b4b2f0f9e7bc1cea6f802282169fea34589
ShubhamR2407 <103954592+ShubhamR2407@users.noreply.github.com>
2023-08-28 13:54:49 +0530

    Update AuthController.js

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
-const { generateToken } = require('../utils/GenerateToken')
+const { generateToken } = require('../utils/generateToken')
 const jwt = require('jsonwebtoken')
 
 // @desc    Register new user
